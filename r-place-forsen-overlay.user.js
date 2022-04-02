8bcfe8a7e6e9bf846ad16d9e711b7b626b400a6c
fuzzball007 <102851690+fuzzball007@users.noreply.github.com>
2022-04-02 19:09:13 +1100

    forsenE

---
+++ r-place-forsen-overlay.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         /r/place forsenE
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  The face of Twitch
+// @author       /u/irate_kalypso and /u/fuzzball007
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @updateURL    https://github.com/fuzzball007/r-place-forsen-overlay/raw/main/r-place-forsen-overlay.user.js
+// @downloadURL  https://github.com/fuzzball007/r-place-forsen-overlay/raw/main/r-place-forsen-overlay.user.js
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://i.imgur.com/rhRuAgZ.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+}
