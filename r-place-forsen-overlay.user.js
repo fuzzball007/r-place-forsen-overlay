02e48a1a68a14f6af0fb66f6b34c6111eec6b8e2
Fuzz Ball 007 <102851690+fuzzball007@users.noreply.github.com>
2022-04-03 17:00:19 +1000

    Change image size

--- r-place-forsen-overlay.user.js
+++ r-place-forsen-overlay.user.js
@@ -16,7 +16,7 @@ if (window.top !== window.self) {
         (function () {
             const i = document.createElement("img");
             i.src = "https://i.ibb.co/s5Xtysb/dotted-place-template-real-1.png";
-            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
             console.log(i);
             return i;
         })())
