c95aeec562d3728a9458ba16ee904999f7a22aac
Fuzz Ball 007 <102851690+fuzzball007@users.noreply.github.com>
2022-04-04 18:31:31 +1000

    Update image and size

--- r-place-forsen-overlay.user.js
+++ r-place-forsen-overlay.user.js
@@ -15,8 +15,8 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://i.nuuls.com/-JB4B.png";
-            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+            i.src = "https://i.nuuls.com/XnOIj.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
         })())
